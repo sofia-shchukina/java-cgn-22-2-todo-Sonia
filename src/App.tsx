42ec6bcf7f01510a028d88e691ddff8b0040b95f
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-07-25 16:40:23 +0200

    Add axios getTodo Request

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,22 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Todo} from "./model/Todo";
 import BoardOverview from "./components/BoardOverview";
+import axios from "axios";
 
 function App() {
 
   const [todos, setTodos] = useState<Todo[]>([]);
+
+  useEffect(() => {
+      getAllTodo()
+  }, [])
+
+  const getAllTodo = () => {
+      axios.get("/api/todo")
+          .then((response) => response.data)
+          .then(setTodos)
+  }
 
   return (
     <div>
