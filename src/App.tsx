918eacd18a308baa54365a8ab1f2fb7a7488aa24
Flooooooooooorian <florian.florianweber.weber@googlemail.com>
2022-07-26 11:26:03 +0200

    Add useTodos Hook

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,12 @@
-import React, {useEffect, useState} from 'react';
+import React from 'react';
 import './App.css';
-import {Todo} from "./model/Todo";
 import BoardOverview from "./components/BoardOverview";
-import axios from "axios";
 import AddTodo from "./components/AddTodo";
-import {getNextStatus} from "./service/todo-service";
+import useTodos from "./hooks/useTodos";
 
 function App() {
 
-  const [todos, setTodos] = useState<Todo[]>([]);
-
-  useEffect(() => {
-      getAllTodo()
-  }, [])
-
-  const getAllTodo = () => {
-      axios.get("/api/todo")
-          .then((response) => response.data)
-          .then(setTodos)
-  }
-
-  const addTodo = (description: string) => {
-      const newTodo = {
-          description: description,
-          status: "OPEN"
-      }
-
-      axios.post("/api/todo", newTodo)
-          .then(getAllTodo)
-  }
-
-  const advanceTodo = (todo: Todo) => {
-      const updatedTodo = {
-          id: todo.id,
-          description: todo.description,
-          status: getNextStatus(todo.status)
-      }
-
-      axios.put(`/api/todo/${todo.id}`, updatedTodo)
-          .then(getAllTodo)
-  }
-
-  const deleteTodo = (id: string) => {
-      axios.delete(`/api/todo/${id}`)
-          .then(getAllTodo)
-  }
+    const {todos, addTodo, advanceTodo, deleteTodo} = useTodos()
 
   return (
     <div>
---
+++ src/hooks/useTodos.ts
@@ -0,0 +1,47 @@
+import {useEffect, useState} from "react";
+import {Todo} from "../model/Todo";
+import axios from "axios";
+import {getNextStatus} from "../service/todo-service";
+
+export default function useTodos() {
+
+    const [todos, setTodos] = useState<Todo[]>([]);
+
+    useEffect(() => {
+        getAllTodo()
+    },  [])
+
+    const getAllTodo = () => {
+        axios.get("/api/todo")
+            .then((response) => response.data)
+            .then(setTodos)
+    }
+
+    const addTodo = (description: string) => {
+        const newTodo = {
+            description: description,
+            status: "OPEN"
+        }
+
+        axios.post("/api/todo", newTodo)
+            .then(getAllTodo)
+    }
+
+    const advanceTodo = (todo: Todo) => {
+        const updatedTodo = {
+            id: todo.id,
+            description: todo.description,
+            status: getNextStatus(todo.status)
+        }
+
+        axios.put(`/api/todo/${todo.id}`, updatedTodo)
+            .then(getAllTodo)
+    }
+
+    const deleteTodo = (id: string) => {
+        axios.delete(`/api/todo/${id}`)
+            .then(getAllTodo)
+    }
+
+    return {todos, addTodo, advanceTodo, deleteTodo}
+}
